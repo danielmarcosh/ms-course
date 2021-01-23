56d313defc32ab915d0397e80081a82f8b5bf8b7
Daniel Marcos <danielmarcosh@gmail.com>
2021-01-23 13:53:23 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/danmarche/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danmarche.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
